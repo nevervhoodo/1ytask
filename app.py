791ae41b6ff495afd1e7c0321fb9c89cb55925cd
nevervhoodo <dkhunter10@gmail.com>
2018-04-11 10:26:44 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import time
+import BaseHTTPServer
+
+
+HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 9000 # Maybe set this to 9000.
+
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(301)
+        s.send_header("Location",'http://unix:/var/run/app.sock')
+        s.end_headers()
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+
